5575f7e3a3a5006329810ddd6f63507b141f50c6
Mitchwatts93 <31891580+Mitchwatts93@users.noreply.github.com>
2019-04-05 15:24:20 +0100

    fixing typing

--- thunderfit/setup.py
+++ thunderfit/setup.py
@@ -8,7 +8,7 @@ def readme():
 setuptools.setup(
     name='thunderfit',
     python_requires='>3.6',
-    version='1.1',
+    version='1.1.1',
     description='Thunderfit fitting code',
     long_description=readme(),
     classifiers=[
