a11e800ed44e14481ed8830484824009f2a388e8
Mitchwatts93 <31891580+Mitchwatts93@users.noreply.github.com>
2019-04-05 10:24:42 +0100

    pypi fixing

--- thunderfit/setup.py
+++ thunderfit/setup.py
@@ -8,7 +8,7 @@ def readme():
 setuptools.setup(
     name='thunderfit',
     python_requires='>3.6',
-    version='1.0.7.12',
+    version='1.0.7.13',
     description='Thunderfit fitting code',
     long_description=readme(),
     classifiers=[
