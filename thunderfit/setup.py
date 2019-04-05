f9fcafb47079e1238aa24034f14280773d277089
Mitchwatts93 <31891580+Mitchwatts93@users.noreply.github.com>
2019-04-05 16:18:36 +0100

    pypiing

--- thunderfit/setup.py
+++ thunderfit/setup.py
@@ -8,7 +8,7 @@ def readme():
 setuptools.setup(
     name='thunderfit',
     python_requires='>3.6',
-    version='1.1.1.6',
+    version='1.1.1.7',
     description='Thunderfit fitting code',
     long_description=readme(),
     classifiers=[
